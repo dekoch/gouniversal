2de1544ecadd4352ed0fce142d217d6cdeb71e84
mario <mario@xps13.workgroup>
2019-08-28 14:05:54 +0200

    +Test: CheckFormInput()

--- shared/functions/functions_test.go
+++ shared/functions/functions_test.go
@@ -1,6 +1,8 @@
 package functions
 
 import (
+	"net/http"
+	"net/url"
 	"testing"
 )
 
@@ -22,6 +24,37 @@ func TestIsEmtpy(t *testing.T) {
 		got := IsEmpty(tt.in)
 		if got != tt.want {
 			t.Errorf("IsEmtpy(): in: %s, got %t, want %t", tt.in, got, tt.want)
+		}
+	}
+}
+
+func TestCheckFormInput(t *testing.T) {
+
+	tests := []struct {
+		inQuery    string
+		inKey      string
+		wantString string
+		wantError  bool
+	}{
+		{"key0=value0", "key0", "value0", false},
+		{"key1=value1 z", "key1", "value1 z", false},
+		{"key2=value2 z<", "key2", "", true},
+	}
+
+	for i, tt := range tests {
+
+		v, _ := url.ParseQuery(tt.inQuery)
+
+		var r http.Request
+		r.Form = v
+
+		gotString, gotError := CheckFormInput(tt.inKey, &r)
+		if gotString != tt.wantString {
+			t.Errorf("CheckFormInput() string test %d: inQuery %s, inKey %s, got %s, want %s", i, tt.inQuery, tt.inKey, gotString, tt.wantString)
+		}
+
+		if (gotError == nil) == tt.wantError {
+			t.Errorf("CheckFormInput() error test %d: inQuery %s, inKey %s, got %v, want %t", i, tt.inQuery, tt.inKey, gotError, tt.wantError)
 		}
 	}
 }
